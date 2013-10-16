c908bae405b0ebd96d919fe5e461c3fa637e0cd2
Ben Nadel <ben@bennadel.com>
2013-10-16 09:46:19 -0400

    Starting to work with Friends and SocialCircles.

--- examples/01-social-circle/js/model/Friend.js
+++ examples/01-social-circle/js/model/Friend.js
@@ -2,12 +2,121 @@ define(
 	[ "util" ],
 	function( util ) {
 
-		// I represent a "specific" person in a social circle.
-		function Friend() {
+		// I represent a "specific" person in a given social circle. This is a "context" in which
+		// the person perform actions.
+		function Friend( aPerson, aSocialCircle ) {
 
-			// ...
+			this.addPerson( aPerson );
+			this.addSocialCircle( aSocialCircle );
 
 		}
+
+
+		// Define the instance methods.
+		Friend.prototype = {
+
+			// I add the given person.
+			addPerson: function( newPerson ) {
+
+				if ( ! newPerson ) {
+
+					throw( new Error( "Person is null." ) );
+
+				}
+
+				this.testAddPerson( newPerson );
+				this.doAddPerson( newPerson );
+
+				// Establish the bi-directional relationship.
+				// newPerson.doAddFriend( this );
+
+			},
+
+
+			// I add the given social circle.
+			addSocialCircle: function( newSocialCircle ) {
+
+				if ( ! newSocialCircle ) {
+
+					throw( new Error( "Social Circle is null." ) );
+
+				}
+
+				this.testAddSocialCircle( newSocialCircle );
+				this.doAddSocialCircle( newSocialCircle );
+				newSocialCircle.doAddFriend( this );
+
+			},
+
+
+			// I add the given person without any validation.
+			doAddPerson: function( newPerson ) {
+
+				this.person = newPerson;
+
+			},
+
+
+			// I add the given social circle without any validation.
+			doAddSocialCircle: function( newSocialCircle ) {
+
+				this.socialCircle = newSocialCircle;
+
+			},
+
+
+			// I test to make sure the given person can be added.
+			testAddPerson: function( newPerson ) {
+
+				if ( this.person ) {
+
+					throw( new Error( "Person cannot be changed." ) );
+
+				}
+
+				// If the social circle has already been set, we can test against it.
+				// --
+				// NOTE: We know that this method will *really* only ever be called before the
+				// social circle has been set (in the constructor); however, that creates a 
+				// connaissance-of-time problem in which the developer has to make sure never to 
+				// change the order of the calls. We can get around this by adding this simple, 
+				// bi-directional check.
+				if ( this.socialCircle ) {
+
+					// When it comes to Social-Circle validation, it's more complex that simply,
+					// "does this friend already exist." As such, we have to defere the entirety
+					// of the new person validation to the social circle.
+					this.socialCircle.testAddPersonConflict( newPerson );
+
+				}
+
+			},
+
+
+			// I test to make sure the given social circle can be added.
+			testAddSocialCircle: function( newSocialCircle ) {
+
+				if ( this.socialCircle ) {
+
+					throw( new Error( "Social Circle cannot be changed." ) );
+
+				}
+
+				// If the person has already been set, we can test against it.
+				// --
+				// NOTE: We know that this method will *really* only ever be called after the
+				// person has been set (in the constructor); however, that creates a connaissance-
+				// of-time problem in which the developer has to make sure never to change the order
+				// of the calls. We can get around this by adding this simple, bi-directional check.
+				if ( this.person ) {
+
+					// ...
+
+				}
+
+			}
+
+		};
 
 
 		// Return the module definition.
--- examples/01-social-circle/js/model/SocialCircle.js
+++ examples/01-social-circle/js/model/SocialCircle.js
@@ -5,9 +5,66 @@ define(
 		// I represent a "collection" of friends.
 		function SocialCircle() {
 
-			// ...
+			// Since the social circle can exist (I think) without friends, we will defer
+			// the population of the friends until after construction. 
+			this.friends = [];
 
 		}
+
+
+		// Define the instance methods.
+		SocialCircle.prototype = {
+
+			// I add the given friend to the social circle.
+			addFriend: function( aFriend ) {
+
+				if ( ! aFriend ) {
+
+					throw( new Error( "Friend is null." ) );
+
+				}
+
+				// Defer the coordination of the "add" to the friend. When streamlining the bi-
+				// directional, the whole defers to part.
+				aFriend.addSocialCircle( this );
+
+			},
+
+
+			// I add the given friend (without any validation).
+			doAddFriend: function( newFriend ) {
+
+				this.friends.push( newFriend );
+
+			},
+
+
+			// I return the collection of friends in the social circle.
+			getFriends: function() {
+
+				// Use slice to make sure the "internal" collection cannot be altered.
+				return( this.friends.slice() );
+
+			},
+
+
+			// I test to see if the given person can be added as a new Friend in the social circle.
+			testAddPersonConflict: function( aPerson ) {
+
+				// Test: Is the person old enough (>18 years).
+				// Test: Does the person already exist in the social circle?
+				// Test: Would this bring us over 150 friends?
+				// Test: Would the gender ratio be violated?
+				// Test: Would a love-triangle be created?
+
+				// THOUGHT: This last test - love triangle - requires that the person's Attractions
+				// be defined *before* they are added to the social circle. To get around this 
+				// contstraint, it means that when an attraction is created for a Person, the Person
+				// has to check all their EXISTING social circles.
+
+			}
+
+		};
 
 
 		// Return the module definition.
